ba7d9822c6978b2f9256de924e559570249ea399
Tokiarivelo <anjaniaina.toky14@gmail.com>
2025-08-18 10:06:48 +0300

    [upload] - add upload component

--- apps/graphql-server/src/main.ts
+++ apps/graphql-server/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3001'], // ton front React
     credentials: true,
   });
 
