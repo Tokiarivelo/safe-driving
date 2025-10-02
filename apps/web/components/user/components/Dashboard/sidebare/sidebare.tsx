ee8ff73b003490843f1984e8dd6dc787684b3cf4
mickaelsma <heritianamickael04@gmail.com>
2025-10-02 12:29:42 +0200

    messager

--- apps/web/components/user/components/Dashboard/sidebare/sidebare.tsx
+++ apps/web/components/user/components/Dashboard/sidebare/sidebare.tsx
@@ -61,138 +61,230 @@ function sidebare() {
         </div>
       </div>
       <div className="bg-white w-12 h-150 space-y-2 rounded-[23px] shadow-auth-card">
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datahome ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger className="p-2 rounded-full" onClick={() => datahome && home()}>
-              <Icon icon="radix-icons:home" width="30" height="30" className="text-[#FF7F41]" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Home</p>
-            </TooltipContent>
-          </Tooltip>
-        </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datarechercher ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger
-              className="p-2 rounded-full"
-              onClick={() => datarechercher && recherche()}
-            >
-              <Icon
-                icon="material-symbols:search-rounded"
-                width="30"
-                height="30"
-                className="text-[#C82BA0]"
-              />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Rechercher un transport</p>
-            </TooltipContent>
-          </Tooltip>
-        </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datamessages ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger className="p-2 rounded-full" onClick={() => datamessages && messages()}>
-              <Icon icon="streamline-ultimate-color:messages-logo" width="30" height="30" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Messages</p>
-            </TooltipContent>
-          </Tooltip>
-          <div className="w-4 h-4 text-[12px] absolute z-20 bg-red-600 ml-7 mb-7 rounded-full text-white flex justify-center items-center">
-            9
+        <div>
+        <Link href="/user/dashboard/dashboard">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datahome ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full cursor-pointer"
+                onClick={() => datahome && home()}
+              >
+                <Icon icon="radix-icons:home" width="30" height="30" className="text-[#FF7F41] cursor-pointer hover:scale-120 transition-transform duration-200" />
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Home</p>
+              </TooltipContent>
+            </Tooltip>
           </div>
+        </Link>
         </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datacourses ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger className="p-2 rounded-full" onClick={() => datacourses && courses()}>
-              <Icon icon="streamline-kameleon-color:map" width="30" height="30" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Mes Courses</p>
-            </TooltipContent>
-          </Tooltip>
-        </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datacourses ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger className="p-2 rounded-full" onClick={() => datascanner && scanner()}>
-              <Icon icon="iconoir:scan-qr-code" width="30" height="30" className="text-[#FF7F41]" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Scanner Qr code</p>
-            </TooltipContent>
-          </Tooltip>
-        </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datanotifications ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger
-              className="p-2 rounded-full"
-              onClick={() => datanotifications && notifications()}
-            >
-              <Icon icon="streamline-sharp-color:bell-notification-flat" width="30" height="30" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Notification</p>
-            </TooltipContent>
-          </Tooltip>
-          <div className="w-4 h-4 absolute z-20 text-[12px] bg-red-600 ml-7 mb-7 rounded-full text-white flex justify-center items-center">
-            3
+        <div>
+        <Link href="/user/dashboard/rechercher">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datarechercher ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full  cursor-pointer"
+                onClick={() => datarechercher && recherche()}
+              >
+                <Icon
+                  icon="material-symbols:search-rounded"
+                  width="30"
+                  height="30"
+                  className="text-[#C82BA0] cursor-pointer hover:scale-120 transition-transform duration-200"
+                />
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Rechercher un transport</p>
+              </TooltipContent>
+            </Tooltip>
           </div>
+        </Link>
         </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datatrajet ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger className="p-2 rounded-full" onClick={() => datatrajet && trajet()}>
-              <Icon icon="icon-park:gps" width="30" height="30" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Trajet en Cours</p>
-            </TooltipContent>
-          </Tooltip>
+        <div>
+        <Link href="/user/dashboard/messages">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datamessages ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full cursor-pointer"
+                onClick={() => datamessages && messages()}
+              >
+                <Icon icon="streamline-ultimate-color:messages-logo" width="30" height="30" className='cursor-pointer hover:scale-120 transition-transform duration-200'/>
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Messages</p>
+              </TooltipContent>
+            </Tooltip>
+            <div className="w-4 h-4 text-[12px] absolute z-20 bg-red-600 ml-7 mb-7 rounded-full text-white flex justify-center items-center">
+              9
+            </div>
+          </div>
+        </Link>
         </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datahome ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger className="p-2 rounded-full" onClick={() => dataprofil && profil()}>
-              <Icon icon="mdi:user" width="30" height="30" className="text-[#C82BA0]" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Profil</p>
-            </TooltipContent>
-          </Tooltip>
+        <div>
+        <Link href="">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datacourses ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full  cursor-pointer"
+                onClick={() => datacourses && courses()}
+              >
+                <Icon icon="streamline-kameleon-color:map" width="30" height="30" className='cursor-pointer hover:scale-120 transition-transform duration-200'/>
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Mes Courses</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </Link>
         </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${dataoffre ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger className="p-2 rounded-full" onClick={() => dataoffre && offre()}>
-              <Icon icon="lsicon:badge-promotion-filled" width="30" height="30" color="red" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Offre Promotions</p>
-            </TooltipContent>
-          </Tooltip>
+        <div>
+        <Link href="">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datacourses ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full  cursor-pointer"
+                onClick={() => datascanner && scanner()}
+              >
+                <Icon
+                  icon="iconoir:scan-qr-code"
+                  width="30"
+                  height="30"
+                  className="text-[#FF7F41] cursor-pointer hover:scale-120 transition-transform duration-200"
+                />
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Scanner Qr code</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </Link>
         </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${dataparametre ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger
-              className="p-2 rounded-full"
-              onClick={() => dataparametre && parametre()}
-            >
-              <Icon icon="uil:setting" width="30" height="30" className="text-[#C82BA0]" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Parametre</p>
-            </TooltipContent>
-          </Tooltip>
+        <div>
+        <Link href="">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datanotifications ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full  cursor-pointer"
+                onClick={() => datanotifications && notifications()}
+              >
+                <Icon icon="streamline-sharp-color:bell-notification-flat" width="30" height="30" className='cursor-pointer hover:scale-120 transition-transform duration-200'/>
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Notification</p>
+              </TooltipContent>
+            </Tooltip>
+            <div className="w-4 h-4 absolute z-20 text-[12px] bg-red-600 ml-7 mb-7 rounded-full text-white flex justify-center items-center">
+              3
+            </div>
+          </div>
+        </Link>
         </div>
-        <div className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datahome ?'border-pink-600':'border-white'}`}>
-          <Tooltip>
-            <TooltipTrigger
-              className="p-2 rounded-full"
-              onClick={() => dataassistance && assistance()}
-            >
-              <Icon icon="flat-color-icons:online-support" width="30" height="30" />
-            </TooltipTrigger>
-            <TooltipContent side="right" align="center">
-              <p>Assistance</p>
-            </TooltipContent>
-          </Tooltip>
+        <div>
+        <Link href="">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datatrajet ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full cursor-pointer"
+                onClick={() => datatrajet && trajet()}
+              >
+                <Icon icon="icon-park:gps" width="30" height="30" className='cursor-pointer hover:scale-120 transition-transform duration-200'/>
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Trajet en Cours</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </Link>
+        </div>
+        <div>
+        <Link href="">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datahome ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full  cursor-pointer"
+                onClick={() => dataprofil && profil()}
+              >
+                <Icon icon="mdi:user" width="30" height="30" className="text-[#C82BA0] cursor-pointer hover:scale-120 transition-transform duration-200" />
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Profil</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </Link>
+        </div>
+        <div>
+        <Link href="">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${dataoffre ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full cursor-pointer"
+                onClick={() => dataoffre && offre()}
+              >
+                <Icon icon="lsicon:badge-promotion-filled" width="30" height="30" color="red" className='cursor-pointer hover:scale-120 transition-transform duration-200'/>
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Offre Promotions</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </Link>
+        </div>
+        <div>
+        <Link href="">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${dataparametre ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full cursor-pointer"
+                onClick={() => dataparametre && parametre()}
+              >
+                <Icon icon="uil:setting" width="30" height="30" className="text-[#C82BA0] cursor-pointer hover:scale-120 transition-transform duration-200" />
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Parametre</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </Link>
+        </div>
+        <div>
+        <Link href="">
+          <div
+            className={`bg-white w-12 h-12 rounded-full border-1 flex justify-center items-center ${datahome ? 'border-pink-600 shadow-md shadow-gray-400' : 'border-white'}`}
+          >
+            <Tooltip>
+              <TooltipTrigger
+                className="p-2 rounded-full  cursor-pointer"
+                onClick={() => dataassistance && assistance()}
+              >
+                <Icon icon="flat-color-icons:online-support" width="30" height="30" className='cursor-pointer hover:scale-120 transition-transform duration-200'/>
+              </TooltipTrigger>
+              <TooltipContent side="right" align="center">
+                <p>Assistance</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </Link>
         </div>
       </div>
     </div>
