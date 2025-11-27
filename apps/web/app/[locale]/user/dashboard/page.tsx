0368cdc8e823a6da16b3412759413ae283f5ba08
Tokiarivelo <anjaniaina.toky14@gmail.com>
2025-11-27 06:45:01 +0300

    [scroll] - pretify scrollbar

--- apps/web/app/[locale]/user/dashboard/page.tsx
+++ apps/web/app/[locale]/user/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import Dashboard from '../../../../components/user/components/Dashboard/dashboard/dashboard';
+import Dashboard from '../../../../components/user/components/dashboard/main/mainPage';
 
 export default function ChoixLoginPage() {
   return <Dashboard />;
